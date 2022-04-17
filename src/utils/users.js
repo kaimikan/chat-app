62df1f5782431e0bc6fe9e8decacd67b60f382b7
kaimikan <oihic@protonmail.com>
2022-04-17 12:58:41 +0200

    Store users

---
+++ src/utils/users.js
@@ -0,0 +1,69 @@
+const users = [];
+
+const addUser = ({ id, username, room }) => {
+  // clean the data
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  // validate the data
+  if (!username || !room) return { error: "Username and room are required." };
+
+  // check for existing user
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username;
+  });
+
+  // validate username
+  if (existingUser) return { error: "Username is in use." };
+
+  // store user
+  const user = { id, username, room };
+  users.push(user);
+  return { user };
+};
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) return users.splice(index, 1)[0];
+};
+
+const getUser = (id) => {
+  const user = users.find((user) => user.id === id);
+  if (user) return user;
+  return { error: "User not found" };
+};
+
+const getUsersInRoom = (roomName) => {
+  roomName = roomName.trim().toLowerCase();
+  const usersInRoom = users.filter((user) => user.room === roomName);
+  if (usersInRoom) return usersInRoom;
+  return { error: "No users found in that room" };
+};
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+};
+
+/* addUser({
+  id: 11,
+  username: "Kai",
+  room: "Deventer",
+});
+
+addUser({
+  id: 22,
+  username: "Jeff",
+  room: "Deventer",
+});
+
+addUser({
+  id: 33,
+  username: "Mike",
+  room: "Hunt",
+});
+
+console.log(getUser(22));
+console.log(getUsersInRoom("DEventer")); */
